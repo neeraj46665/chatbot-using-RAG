1597a4331bd9e8dbfb4a0b19a5b99fc024a99432
Neeraj Singh <100067646+neeraj46665@users.noreply.github.com>
2024-07-07 12:02:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 __import__('pysqlite3') 
-import sys sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sys 
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
